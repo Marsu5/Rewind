c93c671eb4edfcaed80c8cb6fab891eb41f7f04c
Marsu5 <marcell.schuh@gmail.com>
2016-11-24 15:01:57 +0100

    CarActor

--- core/src/hu/tokingame/rewind/Bodies/Car.java
+++ core/src/hu/tokingame/rewind/Bodies/Car.java
@@ -10,16 +10,23 @@ import hu.tokingame.rewind.MyBaseClasses.WorldBodyEditorLoader;
  */
 
 public class Car extends Body {
-    public Car(World world, WorldBodyEditorLoader loader, String bodyID, float X, float Y) {
-        super(world, loader, bodyID, new OneSpriteStaticActor(randomTexture()), X, Y);
+
+    OneSpriteStaticActor actor;
+
+    public Car(World world, WorldBodyEditorLoader loader, float X, float Y) {
+        super(world, loader, "bluecar.png",  new OneSpriteStaticActor(randomTexture()), X, Y);
+        //actor = (OneSpriteStaticActor)getChildren().get(0);
+
     }
 
-    private String randomTexture(){
+    private static String randomTexture(){
         int a = (int)Math.random() * (4-1+1)+4;
         switch (a){
-            case 1 : return "GameTextures/bluecar.png";  break;
-            case 2: return "GameTextures/greencar.png"; break;
-            default: return"GameTextures/bluecar.png"; break;
+            case 1 : return "GameTextures/bluecar.png";
+            case 2 : return "GameTextures/greencar.png";
+            case 3 : return "GameTextures/redcar.png";
+            case 4 : return "GameTextures/bluecar.png";
+            default: return"GameTextures/bluecar.png";
         }
     }
 }
