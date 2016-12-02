235dfce9885727626be776ef0471e58edbd1eca1
Marsu5 <marcell.schuh@gmail.com>
2016-12-02 15:49:39 +0100

    A meglévő irányítások csak akkor jelenek meg a képernyőn
    ha van accelerometer

--- core/src/hu/tokingame/rewind/GameScreen/ControlStage.java
+++ core/src/hu/tokingame/rewind/GameScreen/ControlStage.java
@@ -33,97 +33,99 @@ public class ControlStage extends MyStage {
     public void init() {
 
         if (SettingsStage.onScreenMode = true){
-            //// TODO: 12/1/2016 Csinálj on screen controlt 
+            // TODO: 12/1/2016 Csinálj on screen controlt
+        }else {
+            addActor(Gaspedal = new UpDownButton(Assets.manager.get(Assets.GASPEDAL_UP), Assets.manager.get(Assets.GASPEDAL_DOWN)){
+                @Override
+                public void init() {
+                    super.init();
+                    this.setSize(3.5f, 4);
+                    this.setPosition(12.5f, 0);
+                    addListener(new InputListener(){
+                        @Override
+                        public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                            isGasTouched = true;
+                            pressed();
+                            return super.touchDown(event, x, y, pointer, button);
+                        }
+
+                        @Override
+                        public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                            isGasTouched = false;
+                            realesed();
+                            super.touchUp(event, x, y, pointer, button);
+                        }
+
+                        @Override
+                        public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                            super.exit(event, x, y, pointer, toActor);
+                            realesed();
+                            isGasTouched = false;
+                        }
+                    });
+                }
+            });
+
+            addActor(BrakePedal = new UpDownButton(Assets.manager.get(Assets.BRAKEPEDAL_UP), Assets.manager.get(Assets.BRAKEPEDAL_DOWN)){
+                @Override
+                public void init() {
+                    super.init();
+                    this.setSize(3.5f, 4);
+                    this.setPosition(0, 0);
+                    addListener(new InputListener(){
+                        @Override
+                        public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                            isBrakeTouched = true;
+                            pressed();
+                            return super.touchDown(event, x, y, pointer, button);
+                        }
+
+                        @Override
+                        public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                            super.touchUp(event, x, y, pointer, button);
+                            isBrakeTouched = false;
+                            realesed();
+                        }
+                        @Override
+                        public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                            super.exit(event, x, y, pointer, toActor);
+                            realesed();
+                            isBrakeTouched = false;
+                        }
+                    });
+                }
+            });
+
+            addActor(Turbo = new UpDownButton(Assets.manager.get(Assets.TURBO_OFF), Assets.manager.get(Assets.TURBO_ON)){
+                @Override
+                public void init() {
+                    super.init();
+                    this.setSize(1.5f,1.5f);
+                    this.setPosition(4, 0);
+                    addListener(new InputListener(){
+                        @Override
+                        public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                            turboOn = true;
+                            pressed();
+                            return super.touchDown(event, x, y, pointer, button);
+                        }
+
+                        @Override
+                        public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                            super.touchUp(event, x, y, pointer, button);
+                        }
+
+                        @Override
+                        public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                            super.exit(event, x, y, pointer, toActor);
+                        }
+                    });
+                }
+            });
         }
 
 
-        addActor(Gaspedal = new UpDownButton(Assets.manager.get(Assets.GASPEDAL_UP), Assets.manager.get(Assets.GASPEDAL_DOWN)){
-            @Override
-            public void init() {
-                super.init();
-                this.setSize(3.5f, 4);
-                this.setPosition(12.5f, 0);
-                addListener(new InputListener(){
-                    @Override
-                    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                        isGasTouched = true;
-                        pressed();
-                        return super.touchDown(event, x, y, pointer, button);
-                    }
 
-                    @Override
-                    public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                        isGasTouched = false;
-                        realesed();
-                        super.touchUp(event, x, y, pointer, button);
-                    }
-
-                    @Override
-                    public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
-                        super.exit(event, x, y, pointer, toActor);
-                        realesed();
-                        isGasTouched = false;
-                    }
-                });
-            }
-        });
-
-        addActor(BrakePedal = new UpDownButton(Assets.manager.get(Assets.BRAKEPEDAL_UP), Assets.manager.get(Assets.BRAKEPEDAL_DOWN)){
-            @Override
-            public void init() {
-                super.init();
-                this.setSize(3.5f, 4);
-                this.setPosition(0, 0);
-                addListener(new InputListener(){
-                    @Override
-                    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                        isBrakeTouched = true;
-                        pressed();
-                        return super.touchDown(event, x, y, pointer, button);
-                    }
-
-                    @Override
-                    public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                        super.touchUp(event, x, y, pointer, button);
-                        isBrakeTouched = false;
-                        realesed();
-                    }
-                    @Override
-                    public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
-                        super.exit(event, x, y, pointer, toActor);
-                        realesed();
-                        isBrakeTouched = false;
-                    }
-                });
-            }
-        });
-
-        addActor(Turbo = new UpDownButton(Assets.manager.get(Assets.TURBO_OFF), Assets.manager.get(Assets.TURBO_ON)){
-            @Override
-            public void init() {
-                super.init();
-                this.setSize(1.5f,1.5f);
-                this.setPosition(4, 0);
-                addListener(new InputListener(){
-                    @Override
-                    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                        turboOn = true;
-                        pressed();
-                        return super.touchDown(event, x, y, pointer, button);
-                    }
-
-                    @Override
-                    public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                        super.touchUp(event, x, y, pointer, button);
-                    }
-
-                    @Override
-                    public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
-                        super.exit(event, x, y, pointer, toActor);
-                    }
-                });
-            }
-        });
     }
 
     public UpDownButton getTurbo(){
